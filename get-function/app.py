ac4ac924a1b1896f938a22ae7fc1e0aa70d59b87
Samuel Johnson <email@example.com>
2023-12-19 11:52:59 -0500

    Added SAM template and lambda code

---
+++ get-function/app.py
@@ -0,0 +1,27 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'cloud-resume-challenge'
+
+def lambda_handler(event, context):
+    response = client.update_item(
+            TableName='cloud-resume-challenge',
+            Key = {
+                'stat': {'S': 'view-count'}
+                },
+            UpdateExpression = 'ADD Quantity :inc',
+            ExpressionAttributeValues = {":inc" : {"N": "1"}},
+            ReturnValues = 'UPDATED_NEW'
+            )
+
+    value = {"count": response['Attributes']['Quantity']['N']}
+        
+    return {      
+            'statusCode': 200,
+            "headers": {
+                        'Access-Control-Allow-Origin' : '*',
+                        'Access-Control-Allow-Headers': '*',
+                        'Access-Control-Allow-Credentials': '*',
+                        'Content-Type': 'application/json'
+                        },
+            'body': json.dumps(value)}
